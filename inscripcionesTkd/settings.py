cdddf06c735b8ce0515c03ff57cfb05d0e60b3d4
diegoaldea <diegolucianoaldea@gmail.com>
2021-10-15 17:09:48 -0300

    settings2

--- inscripcionesTkd/settings.py
+++ inscripcionesTkd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ohg2q^k9l)1cafs4jp0c6$@&$mmi6*3%&%%%&6rqu+ly=ztx3v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com/']
 
